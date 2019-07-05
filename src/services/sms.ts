35cce347d7a71084aa8e07d7354c6926e46bb5f0
Ravi Tiwari <ravbi2007@gmail.com>
2019-07-05 11:13:49 +0545

    Changed Sms Request Payload

--- src/services/sms.ts
+++ src/services/sms.ts
@@ -1,44 +1,58 @@
 import http from '../utils/http';
 
 const headers = {
-    'Content-Type': 'application/json',
-    'Client-ID': 'dronasoft-microservice', 
-}
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+  'Client-ID': 'dronasoft-microservice'
+};
 
 export const fetchToken = async (email: string, password: string) => {
-    try{
-        const body = {
-            email, 
-            password
-        }
-        
-        const tokenResponse = await http.post("http://smsapp.easyservice.com.np/api/reseller/login", body)
-
-        return tokenResponse.data.data.token;
-    }catch(error){
-        throw error;
-    }
-}
-
-export const send = async (token: string, mobile: string, message: string, senderId: string) => {
-    const specificHeaders = {
-        headers: {
-            ...headers,
-            'Authorization':`Bearer ${token}`
-        }
-    }
-
+  try {
     const body = {
-        mobile,
-        message,
-        senderId
+      email,
+      password
+    };
+
+    const tokenResponse = await http.post(
+      'http://smsapp.easyservice.com.np/api/reseller/login',
+      body
+    );
+
+    return tokenResponse.data.data.token;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const send = async (
+  token: string,
+  mobile: string,
+  message: string,
+  senderId: string
+) => {
+  const specificHeaders = {
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${token}`
     }
-    
-    try{
-        const sendResponse = await  http.post("http://smsapp.easyservice.com.np/api/client/sms/send", body, specificHeaders)
-        
-        return sendResponse.data.data;
-    }catch(error){
-        throw error;
-    }
-}
+  };
+
+  const body = {
+    contacts: mobile,
+    message,
+    sender_id: senderId,
+    type: 'unicode'
+  };
+
+  try {
+    const sendResponse = await http.post(
+      'http://smsapp.easyservice.com.np/api/client/sms/send',
+      body,
+      specificHeaders
+    );
+
+    return sendResponse.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
